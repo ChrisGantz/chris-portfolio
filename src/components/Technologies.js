4a3f65d840639241e8429e6f6dbb6240df781e08
Chris Gantz <chrisgantz836@gmail.com>
2018-10-03 22:48:44 -0700

    fixed mobile view problem

--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -2,53 +2,55 @@ import React from "react";
 
 export default function Technologies() {
   return (
-    <section className="row technologies">
-      <h2>Technologies</h2>
-      <ul>
-        <li>
-          Languages:
+    <section className="technologies">
+      <div className="row">
+        <h2>Technologies</h2>
+        <ul>
+          <li>
+            Languages:
           <ul>
-            <li>HTML5</li>
-            <li>CSS</li>
-            <li>Javascript(ES6 & JSX)</li>
-          </ul>
-        </li>
-        <p>
-          I can build and style, responsive webpages or app interface from
-          scratch with HTML, CSS ,jQuery. When creating full-stack apps, I do
-          like using modulare JS in ES6 using React as front-end framework.
+              <li>HTML5</li>
+              <li>CSS</li>
+              <li>Javascript(ES6 & JSX)</li>
+            </ul>
+          </li>
+          <p>
+            I can build and style, responsive webpages or app interface from
+            scratch with HTML, CSS ,jQuery. When creating full-stack apps, I do
+            like using modulare JS in ES6 using React as front-end framework.
         </p>
-        <li>
-          Libraries & Framkeworks:
+          <li>
+            Libraries & Framkeworks:
           <ul>
-            <li>jQuery</li>
-            <li>React.js(Redux, Thunk, fetch, router)</li>
-            <li>Node.js</li>
-            <li>Express.js</li>
-          </ul>
-        </li>
-        <li>
-          Other Technologies:
+              <li>jQuery</li>
+              <li>React.js(Redux, Thunk, fetch, router)</li>
+              <li>Node.js</li>
+              <li>Express.js</li>
+            </ul>
+          </li>
+          <li>
+            Other Technologies:
           <ul>
-            <li>NPM</li>
-            <li>RESTful APIs</li>
-            <li>Json</li>
-            <li>AJAX</li>
-            <li>MongoDB</li>
-            <li>Mongoose</li>
-            <li>PostgresSQL</li>
-            <li>Travis CI</li>
-            <li>Heroku</li>
-            <li>GitHub</li>
-          </ul>
-        </li>
-        <p>
-          I can build full-stack apps with RESTful API's that perform CRUD
-          operations using Node.js and Express as my server-side programming. I
-          use MongoDB as my database with Mongoose for the Schema. I can also do
-          SQL as databse.
+              <li>NPM</li>
+              <li>RESTful APIs</li>
+              <li>Json</li>
+              <li>AJAX</li>
+              <li>MongoDB</li>
+              <li>Mongoose</li>
+              <li>PostgresSQL</li>
+              <li>Travis CI</li>
+              <li>Heroku</li>
+              <li>GitHub</li>
+            </ul>
+          </li>
+          <p>
+            I can build full-stack apps with RESTful API's that perform CRUD
+            operations using Node.js and Express as my server-side programming. I
+            use MongoDB as my database with Mongoose for the Schema. I can also do
+            SQL as databse.
         </p>
-      </ul>
+        </ul>
+      </div>
     </section>
   );
 }
--- src/components/projects.js
+++ src/components/projects.js
@@ -2,42 +2,44 @@ import React from "react";
 import writingConcept from "../webPic/landingpage.jpg";
 export default function Projects() {
   return (
-    <section className="project row">
-      <h2>Projects</h2>
-      <ul>
-        <li className="col-6">
-          <h4>Writing Concepts</h4>
-          <img src={writingConcept} />
-          <p>
-            Writing Concepts allows you to create posts about ideas you may have
-            for a story. You can comment like and dislike a post.
+    <section className="project">
+      <div className="row">
+        <h2>Projects</h2>
+        <ul className="projects-list">
+          <li className="col-6 each-project">
+            <h4>Writing Concepts</h4>
+            <img src={writingConcept} />
+            <p>
+              Writing Concepts allows you to create posts about ideas you may have
+              for a story. You can comment like and dislike a post.
           </p>
-        </li>
-        <li className="col-6">
-          <h4>Writing Concepts</h4>
-          <img src={writingConcept} />
-          <p>
-            Writing Concepts allows you to create posts about ideas you may have
-            for a story. You can comment like and dislike a post.
+          </li>
+          <li className="col-6 each-project" >
+            <h4>Writing Concepts</h4>
+            <img src={writingConcept} />
+            <p>
+              Writing Concepts allows you to create posts about ideas you may have
+              for a story. You can comment like and dislike a post.
           </p>
-        </li>
-        <li className="col-6">
-          <h4>Writing Concepts</h4>
-          <img src={writingConcept} />
-          <p>
-            Writing Concepts allows you to create posts about ideas you may have
-            for a story. You can comment like and dislike a post.
+          </li>
+          <li className="col-6 each-project">
+            <h4>Writing Concepts</h4>
+            <img src={writingConcept} />
+            <p>
+              Writing Concepts allows you to create posts about ideas you may have
+              for a story. You can comment like and dislike a post.
           </p>
-        </li>
-        <li className="col-6">
-          <h4>Writing Concepts</h4>
-          <img src={writingConcept} />
-          <p>
-            Writing Concepts allows you to create posts about ideas you may have
-            for a story. You can comment like and dislike a post.
+          </li>
+          <li className="col-6 each-project">
+            <h4>Writing Concepts</h4>
+            <img src={writingConcept} />
+            <p>
+              Writing Concepts allows you to create posts about ideas you may have
+              for a story. You can comment like and dislike a post.
           </p>
-        </li>
-      </ul>
+          </li>
+        </ul>
+      </div>
     </section>
   );
 }
